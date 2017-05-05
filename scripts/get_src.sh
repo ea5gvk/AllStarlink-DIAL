a2c2137e19e61765bfea6dc2e98e241bc7452cc3
Steve N4IRS <szingman@msgstor.com>
2017-05-05 14:42:50 +0000

    Decide which headers to install and use dkms for DAHDI install

--- scripts/get_src.sh
+++ scripts/get_src.sh
@@ -1,7 +1,12 @@
 #! /bin/sh
 
-# Get the kernel-headers for the running kernel
+Get Kernel Headers
+distributor=$(lsb_release -is)
+if [[ $distributor == 'Raspbian' ]]; then
+apt-get install raspberrypi-kernel-headers -y
+elif [[ $distributor == 'Debian' ]]; then
 apt-get install linux-headers-`uname -r` -y
+fi
 
 ###########################################################
 
@@ -18,20 +23,13 @@ echo "1538" >asterisk/.version
 # svn co http://svn.ohnosec.org/svn/projects/allstar/uridiag/trunk uridiag
 git clone https://github.com/AllStarLink/uridiag.git
 
-# Get DAHDI from GitHub
-# As of 8-20-2016 dahdi-linux-complete-current.tar.gz is already in the DL directory.
-# I'll move it when this version of the Bare Metal Installer is official.
-# mkdir -p /srv/download
-# cd /srv/download
-# wget http://downloads.asterisk.org/pub/telephony/dahdi-linux-complete/dahdi-linux-complete-current.tar.gz
-# wget https://github.com/N4IRS/AllStar/raw/master/dahdi-linux-complete-current.tar.gz
-
-# Clean out unneeded source and add dahdi source
+# Clean out unneeded source
 cd /usr/src/astsrc-1.4.23-pre
-# rm -rf libpri
+rm -rf libpri
 rm -rf zaptel
-tar zxvf /srv/download/dahdi-linux-complete-current.tar.gz
-mv dahdi-linux-complete* dahdi-linux-complete
+
+# Install DAHDI
+dpkg-i /srv/dkms/dahdi-linux-complete-dkms_2.10.2+2.10.2_all.deb
 
 ##########################################################
 
