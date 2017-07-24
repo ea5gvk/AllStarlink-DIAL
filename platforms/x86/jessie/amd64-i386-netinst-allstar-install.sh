642403f1de70f734d992b1544f69120f86fed86d
Steve Zingman <N4IRS@users.noreply.github.com>
2017-07-24 10:48:11 -0400

    Update amd64-i386-netinst-allstar-install.sh

--- platforms/x86/jessie/amd64-i386-netinst-allstar-install.sh
+++ platforms/x86/jessie/amd64-i386-netinst-allstar-install.sh
@@ -95,8 +95,8 @@ cp /var/log/install.log /var/log/install_phase1.log
 echo "build DAHDI complete" >>/var/log/install.log
 
 # build libpri
-/srv/scripts/build_libpri.sh
-echo "build libpri complete" >>/var/log/install.log
+# /srv/scripts/build_libpri.sh
+# echo "build libpri complete" >>/var/log/install.log
 
 # patch Asterisk
 /srv/scripts/patch_asterisk.sh
