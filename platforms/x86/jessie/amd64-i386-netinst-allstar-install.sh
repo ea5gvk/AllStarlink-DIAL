2752b433e734e68ee4d67023b40acd134d83d834
Steve Zingman <N4IRS@users.noreply.github.com>
2017-07-24 15:25:09 -0400

    Update amd64-i386-netinst-allstar-install.sh

--- platforms/x86/jessie/amd64-i386-netinst-allstar-install.sh
+++ platforms/x86/jessie/amd64-i386-netinst-allstar-install.sh
@@ -15,12 +15,8 @@ apt-get purge rpcbind -y
 apt-get autoremove -y
 echo "removed NFS and rpcbind" >>/var/log/install.log
 
-passwd -l debian
-
-chage -d 0 root
-
 chage -d 0 repeater
-# echo "Force Password change for root" >>/var/log/install.log
+# echo "Force Password change for repeater" >>/var/log/install.log
 
 
 # Enable and start systemd networking
