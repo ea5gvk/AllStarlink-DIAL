904a1200c9324a769ef3427b8861331888733ed4
root <root@dvswitch.org>
2017-08-04 06:12:49 -0400

    cleanup

--- scripts/required_libs.sh
+++ scripts/required_libs.sh
@@ -9,32 +9,32 @@ set -o errexit
 #                                                   #
 #####################################################
 
+# depends
 apt-get install libusb-dev -y
+
 apt-get install libnewt-dev -y
 apt-get install libeditline0 -y
 apt-get install libncurses5-dev -y
 
-apt-get install bison -y
-
 apt-get install libssl-dev -y
 
 apt-get install libasound2-dev -y
+apt-get install libvorbis-dev -y
 
 apt-get install libcurl4-gnutls-dev -y
 
-apt-get install php5-cli -y
-
 apt-get install libiksemel-dev -y
 
-apt-get install libvorbis-dev -y
+apt-get install libsnmp-dev -y
 
-apt-get install curl -y
-
-# Nice to have utilities and tools
-# is sox required or nice to have
+# recommends utilities and tools
 apt-get install sox -y
 apt-get install usbutils -y
 apt-get install alsa-utils -y
 apt-get install bc -y
 apt-get install dnsutils -y
-apt-get install libsnmp-dev -y
+
+apt-get install bison -y
+apt-get install curl -y
+
+apt-get install php5-cli -y
